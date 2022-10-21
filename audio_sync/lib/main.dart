f14cbc5e940421e930386587dd1c9099f60df8de
TahleeJ <tahlee.jaynes@gmail.com>
2022-10-21 18:18:20 -0400

    add lag frequency starting code

--- audio_sync/lib/main.dart
+++ audio_sync/lib/main.dart
@@ -3,116 +3,96 @@ import 'package:vibration/vibration.dart';
 import 'package:flutter_fft/flutter_fft.dart';
 import 'package:noise_meter/noise_meter.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_fft/flutter_fft.dart';
+
+void main() => runApp(Application());
+
+class Application extends StatefulWidget {
+  @override
+  ApplicationState createState() => ApplicationState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ApplicationState extends State<Application> {
+  double? frequency;
+  String? note;
+  int? octave;
+  bool? isRecording;
 
-  // This widget is the root of your application.
+  FlutterFft flutterFft = new FlutterFft();
+
+  _initialize() async {
+    print("Starting recorder...");
+    // print("Before");
+    // bool hasPermission = await flutterFft.checkPermission();
+    // print("After: " + hasPermission.toString());
+
+    // Keep asking for mic permission until accepted
+    while (!(await flutterFft.checkPermission())) {
+      flutterFft.requestPermission();
+      // IF DENY QUIT PROGRAM
+    }
+
+    // await flutterFft.checkPermissions();
+    await flutterFft.startRecorder();
+    print("Recorder started...");
+    setState(() => isRecording = flutterFft.getIsRecording);
+
+    flutterFft.onRecorderStateChanged.listen(
+            (data) => {
+          // print("Changed state, received: $data"),
+          setState(
+                () => {
+              frequency = data[1] as double,
+              note = data[2] as String,
+              octave = data[5] as int,
+            },
+          ),
+          // flutterFft.setNote = note!,
+          // flutterFft.setFrequency = frequency!,
+          // flutterFft.setOctave = octave!,
+          print(frequency.toString())
+        },
+        onError: (err) {
+          print("Error: $err");
+        },
+        onDone: () => {print("Isdone")});
+  }
+
+  @override
+  void initState() {
+    isRecording = flutterFft.getIsRecording;
+    frequency = flutterFft.getFrequency;
+    note = flutterFft.getNote;
+    octave = flutterFft.getOctave;
+    super.initState();
+    _initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: "Simple flutter fft example",
+        theme: ThemeData.dark(),
+        color: Colors.blue,
+        home: Scaffold(
+          backgroundColor: Colors.purple,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                isRecording!
+                    ? Text("Current note: ${note!},${octave!.toString()}",
+                    style: TextStyle(fontSize: 30))
+                    : Text("Not Recording", style: TextStyle(fontSize: 35)),
+                isRecording!
+                    ? Text(
+                    "Current frequency: ${frequency!.toStringAsFixed(2)}",
+                    style: TextStyle(fontSize: 30))
+                    : Text("Not Recording", style: TextStyle(fontSize: 35))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          ),
+        ));
   }
 }
