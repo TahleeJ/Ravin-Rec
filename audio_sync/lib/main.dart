c5a65bceaffe85143d0077c8970799dd3b3c385d
ei84136 <e.izaguirre0112@example.com>
2022-10-22 12:51:38 -0400

    cool colors in low range freq, move to warm in higher freq

--- audio_sync/lib/main.dart
+++ audio_sync/lib/main.dart
@@ -36,30 +36,55 @@ class ApplicationState extends State<Application> {
 
   final double minMicDb = 45.0;
   final double maxMicDb = 75.0;
+
   final int maxVibe = 255;
+  int destVibe = 0;
+  int currVibe = 0;
+
   final double widthMin = 5.0;
   double widthMax = 300.0;
   double newWidth = 5.0;
+
   final double heightMin = 10.0;
   double heightMax = 300.0;
   double newHeight = 5.0;
-  double newValue = 0.0;
-  double newHue = 0;
+
   double minValue = 0.2;
   double maxValue = 1.0;
+  double newValue = 0.0;
+
   HSVColor hsvColor = HSVColor.fromAHSV(1.0, 217.0, 1.0, 1.0);
+  final double violetHue = 255.0;
+  final double minRedHue = 0.0;
+  final double magentaHue = 255.1;
+  final double maxRedHue = 360.0;
+  double newHue = 0.0;
+
+  double minFreq = 155.0;
+  double maxFreq = 4978.0;
+  double freqScale = 1.0;
+  double splitFreq = 0.0;
+
+  double minDb = 1000.0;
+  double maxDb = 0.0;
+  double currDb = 0.0;
+
+  // double meanDb = 0.0;
+
+  double auxDb = 0.0;
+  double auxDbScale = 0.0;
 
 
-  final int redFreq = 750;
-  final int yellowFreq = 1400;
-  final int greenFreq = 2050;
-  final int cyanFreq = 2700;
-  final int blueFreq = 3350;
-  final int magentaFreq = 4000;
-  int r = 255;
-  int g = 255;
-  int b = 255;
-  double hue = 0.0;
+  // final int redFreq = 750;
+  // final int yellowFreq = 1400;
+  // final int greenFreq = 2050;
+  // final int cyanFreq = 2700;
+  // final int blueFreq = 3350;
+  // final int magentaFreq = 4000;
+  // int r = 255;
+  // int g = 255;
+  // int b = 255;
+  // double hue = 0.0;
 
   _updateHeightAndWidthBasedOnVolume() async {
     newHeight = _newValueInMappedRange(currDb, minMicDb, maxMicDb, heightMin, heightMax);
@@ -105,45 +130,26 @@ class ApplicationState extends State<Application> {
               octave = data[5] as int,
             },
           ),
-              if (frequency! < redFreq) {
-                // r = ((frequency! / redFreq) * 255).floor(),
-                // g = b = 0,
-                hue = (frequency! / redFreq) * 60,
-                // print(((frequency! / redFreq) * 255).floor().toString() + " (" + r.toString() + ", " + g.toString() + ", " + b.toString()
-                print("red")
-              } else if (frequency! < yellowFreq) {
-                // r = 255,
-                // g =  ((frequency! / yellowFreq) * 255).floor(),
-                // b = 0,
-                hue = (frequency! / yellowFreq) * 60 + 61,
-                print("yellow")
-              } else if (frequency! < greenFreq) {
-                // r = 0,
-                // g =  ((frequency! / greenFreq) * 255).floor(),
-                // b = 0,
-                hue = (frequency! / greenFreq) * 60 + 121,
-                print("green")
-              } else if (frequency! < cyanFreq) {
-                // r = 0,
-                // g = 255,
-                // b = ((frequency! / cyanFreq) * 255).floor(),
-                hue = (frequency! / cyanFreq) * 60 + 181,
-                print("cyan")
-              } else if (frequency! < blueFreq) {
-                // r = g = 0,
-                // b = ((frequency! / blueFreq) * 255).floor(),
-                hue = (frequency! / blueFreq) * 60 + 241,
-                print("blue")
-              } else {
-                // r = 255,
-                // g = 0,
-                // b = ((frequency! / magentaFreq) * 255).floor(),
-                hue = (frequency! / magentaFreq) * 60 + 301,
-                print("magenta")
-              },
-              // print(frequency.toString()),
-              hsvColor = hsvColor.withHue(hue),
-              print(hue)
+          // print(frequency.toString()),
+          if (frequency! > maxFreq) {
+            frequency = maxFreq,
+          },
+          if (frequency! < minFreq) {
+            frequency = minFreq,
+          },
+          freqScale = violetHue / maxRedHue,
+          splitFreq = ((maxFreq * freqScale) - minFreq + 1) + minFreq,
+          // print("split is " + splitFreq.toString()),
+          // print("at splitF, should be 360: " + (violetHue + maxRedHue - _newValueInMappedRange(splitFreq, splitFreq, maxFreq, magentaHue, maxRedHue)).toString()),
+          // print("at maxF, should be 255: " + (violetHue + maxRedHue - _newValueInMappedRange(maxFreq +1, splitFreq, maxFreq, magentaHue, maxRedHue)).toString()),
+          if (frequency! >= splitFreq) {
+            newHue = violetHue + maxRedHue - _newValueInMappedRange(frequency!, splitFreq, maxFreq, magentaHue, maxRedHue),
+          } else {
+            newHue = violetHue - _newValueInMappedRange(frequency!, minFreq, splitFreq - .1, minRedHue, violetHue),
+          },
+          // newHue = violetHue - _newValueInMappedRange(frequency!, minFreq, maxFreq, minRedHue, violetHue),
+          hsvColor = hsvColor.withHue(newHue),
+          // print(newHue),
         },
         onError: (err) {
           print("Error: $err");
@@ -172,15 +178,6 @@ class ApplicationState extends State<Application> {
     super.dispose();
   }
 
-  double currDb = 0.0;
-  double maxDb = 0.0;
-  double minDb = 1000.0;
-  double meanDb = 0.0;
-  double auxDb = 0.0;
-  double auxDbScale = 0.0;
-  int currVibe = 0;
-  int destVibe = 0;
-
   void onData(NoiseReading noiseReading) {
     this.setState(() {
       if (!this._isRecording) {
@@ -190,7 +187,7 @@ class ApplicationState extends State<Application> {
 
     // maxDb = max(maxDb, noiseReading.maxDecibel);
     // minDb = min(minDb, noiseReading.meanDecibel);
-    meanDb = noiseReading.meanDecibel;
+    // meanDb = noiseReading.meanDecibel;
     currDb = noiseReading.meanDecibel;
   }
 
